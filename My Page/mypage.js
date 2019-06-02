fd36fc915effeaa3c6bcb2286474e710aa96943f
YanyChoi <51287461+YanyChoi@users.noreply.github.com>
2019-06-02 19:20:20 +0900

    Add files via upload

---
+++ My Page/mypage.js
@@ -0,0 +1,4 @@
+var slide_wrp 		= document.getElementsByClassName("side-menu-wrapper");
+var open_button 	= document.getElementsByClassName("menu-open"); //Menu Open Button
+var close_button 	= document.getElementsByClassName("menu-close"); //Menu Close Button
+var overlay 		= ".menu-overlay"; //Overlay
